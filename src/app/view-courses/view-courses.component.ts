ea23ffa7c9454388b9375e38ed00aedb6b2f1ee9
Rachit7299 <itssunil.agarwal@gmail.com>
2020-11-18 20:02:16 +0530

    'mat-table'

--- src/app/view-courses/view-courses.component.ts
+++ src/app/view-courses/view-courses.component.ts
@@ -3,7 +3,16 @@ import { CourseService } from '../../service/course.service';
 import { ViewSubjectsComponent , SubjectDialogModel } from '../view-subjects/view-subjects.component';
 import { ManageSubjectsComponent , ManageSubjectDialogModel } from '../manage-subjects/manage-subjects.component';
 import { MatDialog } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
 
+
+export interface CourseList {
+  _id:string;
+  name:string;
+  code:string;
+  duration:number;
+  semesters:number;
+}
 @Component({
   selector: 'app-view-courses',
   templateUrl: './view-courses.component.html',
@@ -13,11 +22,16 @@ export class ViewCoursesComponent implements OnInit {
 
   constructor(private dialog: MatDialog,private apiService:CourseService) { }
 
-  courses=[];
+  courses:CourseList[];
   page:number=1;
   admin:boolean;
+  total=0;
+  tableData:CourseList[];
+  displayedColumns: string[] = ['name', 'code', 'duration', 'semester', 'actions'];
+  isLoading=false;
 
   ngOnInit(): void {
+    this.isLoading=true;
     this.admin=false;
     this.apiService.getallcourses().subscribe(
       (res)=>{
@@ -28,12 +42,32 @@ export class ViewCoursesComponent implements OnInit {
         else{
           this.admin=false;
         }
+        this.total=this.courses.length;
+        if(this.total>=10){
+          this.tableData= this.courses.slice(0,10);
+        }
+        else{
+          this.tableData= this.courses.slice(0,this.total);
+          
+        }
+        this.isLoading=false;
       },(err)=>{
         if(err.status!=200){
           window.alert('Error Loading Courses!')
         }
+        this.isLoading=false;
       }
     )
+  }
+
+
+  OnpageEvent(event:PageEvent){
+    const StartIndex = event.pageIndex * event.pageSize;
+    let endIndex = StartIndex + event.pageSize;
+    if(endIndex > this.total){
+      endIndex = this.total;
+    }
+    this.tableData = this.courses.slice(StartIndex, endIndex);
   }
 
   viewSubjects(id){
